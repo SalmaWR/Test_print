bfb96dc41227d8c18345204a5d5ebdfedfbdeaa5
SalmaWR <Salma.wagdy@hotmail.com>
2023-03-25 21:54:51 +0200

    updated

---
+++ _functions.c
@@ -0,0 +1,31 @@
+#include"header.h"
+
+/**
+* print_string - Print string
+* @list: list.
+*
+* Return: String length.
+**/
+
+int print_string(va_list args)
+{
+
+char *str = va_arg(args, char*);
+int size = _strlen(str);
+
+return (write(STDOUT_FILENO, str, size));
+}
+
+/**
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+
+int print_char(va_list args)
+{
+char c =  va_arg(args, int);
+return (write(STDOUT_FILENO, &c, 1));
+}
+
--- header.h
+++ header.h
@@ -4,13 +4,20 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<stdarg.h>
-
+ 
+/* printf.c */
 int _printf(const char *format, ...);
 
+/* utils.c */
 int _strlen(const char *format);
 
 int string_printer(const char *format, va_list args);
 
-int Specifier_handler(const char *format, va_list args, int *i);
+int percent_handler(const char *format, va_list args, int *i);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
 
 #endif
