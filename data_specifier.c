5981b26042563d833db67976be46bcf689c1702a
SalmaWR <Salma.wagdy@hotmail.com>
2023-03-25 16:21:26 +0200

    matrioska_Function

---
+++ data_specifier.c
@@ -0,0 +1,59 @@
+#include"header.h"
+
+/**
+* data_specidier - Format cotroller 
+* @format: String format
+* @args  : list of arguments
+*
+* Return : Total size of arguments with the total size of the base string 
+**/
+
+
+int string_printer(const char *format, va_list args)
+
+{
+
+int i = 0;
+int string_size = 0;
+
+int (*Format_specifier)(format, args, &i) = Percent_handler;
+
+	for (i =0; format[i] !=  0; i++)
+	{
+		if (format[i] = '%')
+		
+		{
+		i++
+		
+		 
+		}
+		
+		else
+		
+		{
+			write(1, format[i], 1);
+			string_size += 1;
+		}
+
+		
+	}
+		return (string_size);
+}
+
+
+
+
+/**
+* percent_handler - Controller for percent format
+* @format: String format
+* @args  : List of arguments
+* @i     : Iterator
+* Return: Size of the numbers of elements printed
+*/
+
+int Percent_handler(const char *format, va_list args, int i)
+{
+
+}
+
+
