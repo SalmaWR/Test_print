96c8df4d1236b36e510a29ebbed9be7b25ce8270
SalmaWR <Salma.wagdy@hotmail.com>
2023-03-25 19:45:16 +0200

    matrioska_Function

--- data_specifier.c
+++ data_specifier.c
@@ -60,8 +60,30 @@ int (*Format_specifier)(const char *format,va_list args, &i) = Percent_handler;
 int Percent_handler(const char *format, va_list args, int *i)
 {
 
-/* */
+if (format[*i] = '%')
+write(1, format[*i], 1);
 
 
+/* intializ pointer that point to print functions*/
+int (*definer)(va_list args);
+
+if ( format[*i] = 's')
+{
+definer = &print_string;
+  
+} 
+
+else if (format[*i] = 'c') 
+{
+definer = &print_char;
+} 
+
+else if (format[*i] = 'd') 
+
+{ 
+definer = &print_string; 
 }
+
+definer(args);
+
 
